de13947a14a88ad2b5d8d5fd671e87b4eabcf63d
Matheus Weber <matheusweber@Matheuss-MacBook-Air.local>
2018-02-11 22:21:10 -0200

    Game Detail done

--- TopGames/Features/Game Detail/GameDetailViewController.swift
+++ TopGames/Features/Game Detail/GameDetailViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import SwiftSpinner
 
 class GameDetailViewController: UIViewController {
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var channelLabel: UILabel!
+    @IBOutlet weak var viewersLabel: UILabel!
+    @IBOutlet weak var boxImageView: UIImageView!
+    
     var topModel: TopModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigation()
+        loadData()
         // Do any additional setup after loading the view.
     }
 
@@ -26,5 +33,39 @@ class GameDetailViewController: UIViewController {
     func configureNavigation() {
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         title = topModel?.game.localizedName
+    }
+    
+    func loadData() {
+        if let topModel = topModel {
+            channelLabel.text = "\(topModel.channels) canais"
+            viewersLabel.text = "\(topModel.viewers) visualizações"
+            
+            topModel.game.getImage(type: .logo) { (image) in
+                UIView.transition(with: self.logoImageView,
+                                  duration: 0.5,
+                                  options: UIViewAnimationOptions.transitionCrossDissolve,
+                                  animations: { self.logoImageView.image = image },
+                                  completion: nil)
+                if isReachable {
+                    topModel.game.getImage(type: .box) { (image) in
+                        UIView.transition(with: self.boxImageView,
+                                          duration: 0.5,
+                                          options: UIViewAnimationOptions.transitionCrossDissolve,
+                                          animations: { self.boxImageView.image = image },
+                                          completion: nil)
+                        SwiftSpinner.hide()
+                    }
+                } else {
+                    SwiftSpinner.hide()
+                }
+                
+            }
+            
+        } else {
+            channelLabel.text = ""
+            viewersLabel.text = ""
+        }
+        
+        SwiftSpinner.show("Loading...")
     }
 }
